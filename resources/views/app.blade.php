0729f559c30b38d069adf538d4812f2e945c4e40
Sebastian Morales <sebasmorales05col@gmail.com>
2024-02-16 13:11:44 -0500

    fix app in prod

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,10 +1,21 @@
+@php
+use Illuminate\Support\Facades\Vite;
+@endphp
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	@viteReactRefresh
-	@vite('resources/js/app.jsx', 'build/')
+	@production
+		<script>
+			{!! Vite::content('resources/js/app.jsx') !!}
+		</script>
+	@else
+		@vite('resources/js/app.jsx')
+	@endproduction
+
 	@inertiaHead
 </head>
 <body>
