94b398cc6a8f01e9c692b33711bdef060153c50d
Sebastian Morales <sebasmorales05col@gmail.com>
2024-02-16 13:17:36 -0500

    restore

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,14 +8,7 @@ use Illuminate\Support\Facades\Vite;
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	@viteReactRefresh
-	@production
-		<script>
-			{!! Vite::content('resources/js/app.jsx') !!}
-		</script>
-	@else
-		@vite('resources/js/app.jsx')
-	@endproduction
-
+	@vite('resources/js/app.jsx', 'build/')
 	@inertiaHead
 </head>
 <body>
