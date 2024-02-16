ce51b46fee7d6d48516a32d79497ec5990772899
Sebastian Morales <sebasmorales05col@gmail.com>
2024-02-16 14:01:52 -0500

    add ssr

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,7 +2,7 @@
 import '../css/app.css'
 
 import { createInertiaApp } from '@inertiajs/react'
-import { createRoot } from 'react-dom/client'
+import { hydrateRoot } from 'react-dom/client'
 
 createInertiaApp({
   resolve: name => {
@@ -10,6 +10,6 @@ createInertiaApp({
     return pages[`./Pages/${name}.jsx`]
   },
   setup({ el, App, props }) {
-    createRoot(el).render(<App {...props} />)
+    hydrateRoot(el, <App {...props} />)
   },
 })
